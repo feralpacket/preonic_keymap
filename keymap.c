ed8c25c2abf726865f65fc58fd93b0f49926f602
feralpacket <59261671+feralpacket@users.noreply.github.com>
2021-06-16 10:30:50 -0500

    LOWER and  RAISE send a space when tapped

--- keymap.c
+++ keymap.c
@@ -30,8 +30,8 @@ enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
   GAME,
   NUMPAD,
-  LOWER,
-  RAISE,
+  LOWER = LT(_LOWER, KC_SPC),
+  RAISE = LT(_RAISE, KC_SPC),
   BACKLIT,
   SRS
 };
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   KC_NO,   KC_NO,   KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, KC_NO,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
   KC_NO,   KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_NO,   KC_NO,   KC_MPRV, KC_MUTE, KC_NO,   KC_ENT,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,      KC_NO,       KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,      KC_SPC,      KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
  /* Raise
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
   KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS, KC_NO,   KC_NO,   KC_NO,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   KC_NO,   KC_INS,  KC_HOME, KC_PGUP, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MPRV, KC_MUTE, KC_NO,   KC_ENT,
-  KC_NO,   KC_DEL,  KC_END,  KC_PGDN, KC_TRNS,     KC_NO,        KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_NO,   KC_DEL,  KC_END,  KC_PGDN, KC_TRNS,     KC_SPC,       KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Adjust (Lower + Raise)
@@ -161,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,   QWERTY,  MU_MOD,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF,LALT(KC_PSCR), LCTL(KC_PSCR), KC_PSCR,
   KC_CAPS, MUV_DE,  MUV_IN,  KC_NO,   KC_LEAD, GAME,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   KC_NO,   KC_NO,   KC_NO,   RGB_TOG, RGB_MOD, SRS,     NUMPAD,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,      KC_SPC,      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO
 )
 
 };
@@ -194,7 +194,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             layer_off(_LOWER);
             update_tri_layer(_LOWER, _RAISE, _ADJUST);
           }
-          return false;
+          // return false;
           break;
         case RAISE:
           if (record->event.pressed) {
@@ -204,7 +204,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             layer_off(_RAISE);
             update_tri_layer(_LOWER, _RAISE, _ADJUST);
           }
-          return false;
+          // return false;
           break;
         case BACKLIT:
           if (record->event.pressed) {
