95f188f8d7b95265dc751eb9bea46d1f2bec1306
feralpacket <59261671+feralpacket@users.noreply.github.com>
2021-06-24 10:10:05 -0500

    Added DOT and COMM to the RAISE and LOWER layers

--- keymap.c
+++ keymap.c
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |  Esc |      |  Ins | Home |PG UP |   4  |   5  |   6  |   +  |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|      |  Del |  End |PG DN |   1  |   2  |   3  |   =  |      |      |SftEnt|
+ * | Shift|      |  Del |  End |PG DN |   1  |   2  |   3  |   =  |   .  |      |SftEnt|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |      |      |  Alt |Lower |      0      |Raise | Left | Down |  Up  | Right|
  * `-----------------------------------------------------------------------------------'
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,   KC_NO,   KC_NO,   KC_DEL,
   KC_TAB,  KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_NO,   KC_NO,   KC_BSPC,
   KC_ESC,  KC_NO,   KC_INS,  KC_HOME, KC_PGUP, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_NO,   KC_NO,   KC_NO,
-  KC_LSFT, KC_NO,   KC_DEL,  KC_END,  KC_PGDN, KC_P1,   KC_P2,   KC_P3,   KC_EQL,  KC_NO,   KC_NO,   KC_SFTENT,
+  KC_LSFT, KC_NO,   KC_DEL,  KC_END,  KC_PGDN, KC_P1,   KC_P2,   KC_P3,   KC_EQL,  KC_DOT,  KC_NO,   KC_SFTENT,
   KC_LCTL, KC_NO,   KC_NO,   KC_LALT, LOWER,        KC_P0,       RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |M BTN1| M UP |M BTN2|M BTN3|      |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |M LEFT| M DN |M RGT |      |      |      | Prev |      |      | Enter|
+ * |      |      |M LEFT| M DN |M RGT |      |      |      |   ,  |   .  |      | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   KC_NO,   KC_NO,   KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, KC_NO,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-  KC_NO,   KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_NO,   KC_NO,   KC_MPRV, KC_MUTE, KC_NO,   KC_ENT,
+  KC_NO,   KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_NO,   KC_NO,   KC_COMM, KC_DOT,  KC_NO,   KC_ENT,
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,      KC_SPC,      KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -126,20 +126,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |PRTSC |SCRLK |Pause |      |      |      |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      | Ins  | Home |PG UP |      |      |      |      | Prev |      |      | Enter|
+ * |      | Ins  | Home |PG UP |      |      |      |      |   ,  |   .  |      | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | Del  | End  |PG DN |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_1x2uC(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_NO,   KC_PSCR, KC_SLCK, KC_PAUS, KC_NO,   KC_NO,   KC_NO,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  KC_NO,   KC_INS,  KC_HOME, KC_PGUP, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MPRV, KC_MUTE, KC_NO,   KC_ENT,
+  KC_NO,   KC_INS,  KC_HOME, KC_PGUP, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_COMM, KC_DOT,  KC_NO,   KC_ENT,
   KC_NO,   KC_DEL,  KC_END,  KC_PGDN, KC_TRNS,     KC_SPC,       KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
